be90d74eb28794af95cf81d9843331ef3a7764f0
Riley1800 <c20426892@mytudublin.ie>
2025-04-29 10:03:35 +0100

    Started movement script in drone.gd

--- drone.gd
+++ drone.gd
@@ -5,17 +5,21 @@ extends RigidBody3D
 @export var forward_force:Vector3
 @export var backward_force:Vector3
 
+@export var speed = 10
 @export var max_speed = 10
+
 
 @export var example_enabled = false
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass # Replace with function body.
 
 func _process(delta):
 
-	var turn = Input.get_axis("left", "right") - v.x	
-			if abs(turn) > 0:   
-				position = position + global_transform.basis.x * speed * turn * mult * delta
-				# global_translate(global_transform.basis.x * speed * turn * mult * delta)
+	var up = Input.get_axis("up", 0)
+	if abs(up) > 0:     
+		global_translate(- global_transform.basis.y * speed * up *  delta)
+
+	var turn = Input.get_axis("left", "right")
+	if abs(turn) > 0:   
+		position = position + global_transform.basis.x * speed * turn *  delta
