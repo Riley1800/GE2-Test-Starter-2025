d58f26fd06a62acd3b51fdbb4135a31889e27e84
Riley1800 <c20426892@mytudublin.ie>
2025-04-29 10:57:00 +0100

    arm to prop and propellers now rotate

---
+++ arm_to_prop.gd
@@ -0,0 +1,11 @@
+extends CSGCylinder3D
+
+@export var rot_speed = 1000
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass # Replace with function body.
+	if Input.is_action_pressed("up"):
+		rotate_y(- deg_to_rad(rot_speed) * delta)
